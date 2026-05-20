601e6a3850c4b99dfade55082d3cb59cb3bd3c34
DHANVANT22 <155830846+DHANVANT22@users.noreply.github.com>
2026-05-20 13:12:08 +0530

    Remove unused analytics and templates files

--- apps/analytics/analytics/serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from .models import DailyAnalytics
-
-
-class DailyAnalyticsSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = DailyAnalytics
-        fields = '__all__'
